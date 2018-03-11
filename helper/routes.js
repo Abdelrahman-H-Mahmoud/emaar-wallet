a730896e9a8cd49e4f853605b2b03464cf3f2f27
Abdo <abdo38013@gmail.com>
2018-03-11 14:30:15 +0200

    Money Module

--- helper/routes.js
+++ helper/routes.js
@@ -3,4 +3,5 @@ module.exports = function loadRoutes(app) {
     app.use('/api/user', require('../routes/user'));
     app.use('/api', require('../routes/api'));
     app.use('/api/transaction', require('../routes/transaction'));
+    app.use('/api/money', require('../routes/money'));
 }
