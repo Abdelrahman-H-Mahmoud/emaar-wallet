08322695c18591350ffe2adc745a2134b0d6ab55
Abdo <abdo38013@gmail.com>
2018-03-12 13:23:00 +0200

    remove console.log from env-config

--- config/env-config.js
+++ config/env-config.js
@@ -4,7 +4,6 @@ let config=require(`./${env}.json`);
 if(env==="production"){
     config.databaseConnection=config.databaseConnection.replace("<password>",process.env.dbPassword)
                                                        .replace("<username>",process.env.dbUserName);
-    console.log(config.databaseConnection);
 }
 config.port=process.env.PORT || 3000
 module.exports=config;
