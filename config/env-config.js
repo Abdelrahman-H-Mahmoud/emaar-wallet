557a58b66bd7016f09807e1b3d78a9a8b56705d5
Abdo <abdo38013@gmail.com>
2018-03-12 13:13:24 +0200

    add username and password for db at runtime

--- config/env-config.js
+++ config/env-config.js
@@ -1,5 +1,9 @@
 let env=process.env.NODE_ENV || 'local';
 
-let config=require(`./${env}.json`)
+let config=require(`./${env}.json`);
+if(env==="production"){
+    config.databaseConnection.replace("<username>",process.env.dbUserName);
+    config.databaseConnection.replace("<password>",process.env.dbPassword);
+}
 config.port=process.env.PORT || 3000
 module.exports=config;
