4412aa2114aff85a22f4cd9cce9e6fc8a44b4d15
Abdo <abdo38013@gmail.com>
2018-03-12 13:21:50 +0200

    replace username and password bug fix

--- config/env-config.js
+++ config/env-config.js
@@ -2,9 +2,8 @@ let env=process.env.NODE_ENV || 'local';
 
 let config=require(`./${env}.json`);
 if(env==="production"){
-    console.log(process.env.dbUserName);
-    config.databaseConnection.replace("<username>",process.env.dbUserName);
-    config.databaseConnection.replace("<password>",process.env.dbPassword);
+    config.databaseConnection=config.databaseConnection.replace("<password>",process.env.dbPassword)
+                                                       .replace("<username>",process.env.dbUserName);
     console.log(config.databaseConnection);
 }
 config.port=process.env.PORT || 3000
