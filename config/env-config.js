3f7fb9fea49d290fb357855af233c7de5873fbd4
Abdo <abdo38013@gmail.com>
2018-03-12 16:33:21 +0200

    get secret at run time

--- config/env-config.js
+++ config/env-config.js
@@ -5,6 +5,7 @@ if(env==="production"){
     config.databaseConnection=
     config.databaseConnection.replace("<password>",process.env.dbPassword)
                              .replace("<username>",process.env.dbUserName);
+    config.secret=process.env.SECRET;
 }
 config.port=process.env.PORT || 3000
 module.exports=config;
