032dc9d42dff9cc2455c1062ace87c59b6febd42
Abdo <abdo38013@gmail.com>
2018-03-12 16:26:55 +0200

    format file

--- config/env-config.js
+++ config/env-config.js
@@ -2,8 +2,9 @@ let env=process.env.NODE_ENV || 'local';
 
 let config=require(`./${env}.json`);
 if(env==="production"){
-    config.databaseConnection=config.databaseConnection.replace("<password>",process.env.dbPassword)
-                                                       .replace("<username>",process.env.dbUserName);
+    config.databaseConnection=
+    config.databaseConnection.replace("<password>",process.env.dbPassword)
+                             .replace("<username>",process.env.dbUserName);
 }
 config.port=process.env.PORT || 3000
 module.exports=config;
